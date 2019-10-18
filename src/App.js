5cddc506d4b37d47fa31702d2a532f38daecdec5
mayfielc <christopher.mayfield72@gmail.com>
2019-10-17 19:57:49 -0500

    api key added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Titles from "./components/Titles";
+import Weather from "./components/Weather";
+import Form from "./components/Form";
+
+const API_KEY = "0076b79db631be3626f216ece85c5e7a";
+
+
+class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Titles />
+          <Form />
+          <Weather />
+
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
